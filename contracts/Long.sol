ea9ec0c61217ee16a27b4b2936cbc0d297a1a711
thegostep <thegostep@gmail.com>
2021-02-03 15:15:28 +0000

    add snapshot compatibility

--- contracts/Long.sol
+++ contracts/Long.sol
@@ -4,11 +4,12 @@ pragma solidity 0.7.6;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import {ERC20Snapshot} from "@openzeppelin/contracts/token/ERC20/ERC20Snapshot.sol";
 
 // todo: add permit interface
 // todo: make inflation continuous
 // todo: use unicode symbol ♾️
-contract Long is ERC20("Long", "unicode"), ERC20Burnable {
+contract Long is ERC20("Long", "unicode"), ERC20Burnable, ERC20Snapshot {
     /* storage */
 
     address public immutable tokenManager;
@@ -37,9 +38,21 @@ contract Long is ERC20("Long", "unicode"), ERC20Burnable {
     function advance() external {
         // require new epoch
         require(block.timestamp >= epochStart + epochLength, "Long: not ready to advance");
+        // create snapshot
+        ERC20Snapshot._snapshot();
         // calculate inflation amount
         uint256 amount = (ERC20.totalSupply() * inflationPts) / 10000;
         // mint to tokenManager
         ERC20._mint(tokenManager, amount);
+    }
+
+    /* hook functions */
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Snapshot) {
+        ERC20Snapshot._beforeTokenTransfer(from, to, amount);
     }
 }
